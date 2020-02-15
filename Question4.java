54c767671e8313f4ca6c189c430443d899f70180
Mohammad el kaswani <mohammad-kiswani@outlook.com>
2020-02-15 14:34:27 -0500

    The  program that prompts the user for a URL

---
+++ Question4.java
@@ -0,0 +1,15 @@
+public class Question4 {
+
+    public static void main(String[] args) {
+
+        System.out.println("****URL****");
+
+        int URL;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the valid url :    ");
+
+        System.out.println("Is this a valid and secure URL?")
+    }
+
+}
